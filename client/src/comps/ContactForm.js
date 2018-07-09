aaefe607d561eeabe26c3286cfb13ec33dcf5e5e
n0278705 <casey.trombley@libertymutual.com>
2018-07-09 09:31:18 -0400

    first

---
+++ client/src/comps/ContactForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+
+class ContactForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    async handleSubmit(e) {
+        e.preventDefault();
+
+        const { name, email, message } = this.state;
+
+        const form = await axios.post('/api/form', {
+            name,
+            email,
+            message
+        })
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                    <Label for="name">Name:</Label>
+                    <Input
+                        type="text"
+                        for="name"
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="email">Email:</Label>
+                    <Input
+                        type="text"
+                        for="email"
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="message">Message:</Label>
+                    <Input
+                        type="textarea"
+                        for="message"
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <Button type="submit">Submit</Button>
+            </Form>
+        );
+    }
+}
+
+export default ContactForm;
